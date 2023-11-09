e0420c88ac52a4be022f62381136ca247a1674d9
RenShadecrest <michaelaaronphoto@gmail.com>
2023-11-09 13:21:28 -0800

    continued vercel knex troubleshooting

--- src/db/connections.ts
+++ src/db/connections.ts
@@ -1,5 +1,6 @@
+require("dotenv").config();
 const env = process.env.NODE_ENV || "development";
-const config = require("./knexFile")[env];
+const config = require("./knexfile")[env];
 const knex = require("knex")(config);
  
 export default knex;
