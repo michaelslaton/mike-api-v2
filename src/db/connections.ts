3f84e86eb3315369bf9bb4a8573c9ffda06379eb
RenShadecrest <michaelaaronphoto@gmail.com>
2023-11-09 13:08:05 -0800

    continued knex troubleshooting

--- src/db/connections.ts
+++ src/db/connections.ts
@@ -1,6 +1,5 @@
-require("dotenv").config();
 const env = process.env.NODE_ENV || "development";
-const config = require("../../knexFile")[env];
+const config = require("./knexFile")[env];
 const knex = require("knex")(config);
  
 export default knex;
